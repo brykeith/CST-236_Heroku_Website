0d9b60623af8356ef0dacaa1ae2b55206f215fab
Brydon Johnson <brykeith@gmail.com>
2021-03-08 19:43:41 -0700

    removed echo testing lines from autoloader

--- autoLoader.php
+++ autoLoader.php
@@ -14,18 +14,18 @@
 
 spl_autoload_register(function ($class) {
 
-  echo '<br><br>';
+  // echo '<br><br>';
   // get the difference in the folders between the location
   // of autoloader and the file that called it
   $lastdirectories = substr(getcwd(), strlen(__DIR__));
-  echo '<br>getcwd = :' . getcwd() . '<br>';
-  echo '__DIR__ = : ' . __DIR__ . '<br>';
-  echo 'last directories = : ' . $lastdirectories . '<br>';
+  // echo '<br>getcwd = :' . getcwd() . '<br>';
+  // echo '__DIR__ = : ' . __DIR__ . '<br>';
+  // echo 'last directories = : ' . $lastdirectories . '<br>';
 
   // count the number of slashes (folder depth)
   $numberOfLastDirectories = substr_count($lastdirectories, '/');
 
-  echo '<br> number of last directories: ' . $numberOfLastDirectories . '<br>';
+  // echo '<br> number of last directories: ' . $numberOfLastDirectories . '<br>';
 
   // the list of possible locations that classes are found in this app
   $directories = [
@@ -42,26 +42,26 @@ spl_autoload_register(function ($class) {
   // look inside each directory for the desired class.
   foreach ($directories as $d) {
     $currentDirectory = $d;
-    echo '<br> line 45 ' . $currentDirectory . '<br>';
+    // echo '<br> line 45 ' . $currentDirectory . '<br>';
     for ($x = 0; $x < $numberOfLastDirectories; $x++) {
       $currentDirectory = "../" . $currentDirectory;
-      echo '<br> line 48 ' . $currentDirectory . '<br>';
+      // echo '<br> line 48 ' . $currentDirectory . '<br>';
     }
     $classfile = $currentDirectory . '/' . $class . '.php';
-    echo '<br> line 51 ' . $classfile . '<br>';
+    // echo '<br> line 51 ' . $classfile . '<br>';
 
 
     if (is_readable($classfile)) {
-      echo '<br> line 55 . classfile is readable' . $classfile . '<br>';
+      // echo '<br> line 55 . classfile is readable' . $classfile . '<br>';
       if (require $d . '/' . $class . ".php") {
-        echo '<br> line 57: required ' . $d . '/' . $class . '.php successfully <br>';
+        // echo '<br> line 57: required ' . $d . '/' . $class . '.php successfully <br>';
 
         break;
       } else {
-        echo '<br> line 57: did NOT reuire ' . $d . '/' . $class . '.php successfully  <br>';
+        // echo '<br> line 57: did NOT reuire ' . $d . '/' . $class . '.php successfully  <br>';
       }
     } else {
-      echo '<br>' . $classfile . ' is not readable.';
+      // echo '<br>' . $classfile . ' is not readable.';
     }
   }
 });
